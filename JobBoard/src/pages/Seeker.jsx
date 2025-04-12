d79e9af9da10bf091f859236aef5f3244cc26e13
Yarin <yarinleib@gmail.com>
2025-04-12 16:40:55 +0200

    back button

--- JobBoard/src/pages/Seeker.jsx
+++ JobBoard/src/pages/Seeker.jsx
@@ -1,8 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 export function Seeker() {
+  const navigate = useNavigate();
   return (
     <>
       <div>
         <h1 className='text-warning text-center'>JobBoard </h1>
+        <button className='btn btn-primary' onClick={() => navigate('/')}>
+          Go back
+        </button>
       </div>
     </>
   );
