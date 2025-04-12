ccc90a4e4e61f95a2aacf27be1cb9457a9bbbe12
Yarin <yarinleib@gmail.com>
2025-04-12 16:48:05 +0200

    logo and navigation

--- JobBoard/src/pages/HomePage.jsx
+++ JobBoard/src/pages/HomePage.jsx
@@ -1,20 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 export function HomePage() {
+  const navigate = useNavigate();
   return (
     <div>
-       <div className="p-3 mb-4 bg-secondary d-flex align-items-center justify-content-between">
-    <h1 className="text-warning text-center m-0 flex-grow-1">JobBoard</h1>
-    <img
-      src="/public/logo.png"
-      alt="Logo"
-      style={{ height: "40px", width: "40px", objectFit: "contain" }}
-    />
+      <div className='p-3 mb-4 bg-secondary d-flex align-items-center justify-content-between'>
+        <h1 className='text-warning text-center m-0 flex-grow-1'>JobBoard</h1>
+        <img style={{ width: '55px', height: 'auto' }} src='/public/logo.png' />
       </div>
       <div>
         <h2>Welcome</h2>
         <p>Welcome to the JobBoard application!</p>
         <h2>Are you a</h2>
-        <h3>Employer</h3>
-        <h3>Job Seeker</h3>
+        <button onClick={() => navigate('/employer')}>Employer</button>
+        <button onClick={() => navigate('/seeker')}>Job Seeker</button>
       </div>
     </div>
   );
