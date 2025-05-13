7d0e1fdd9dfd57b9d215b0a73a58159395b3c9ce
Yarin <yarinleib@gmail.com>
2025-05-13 19:33:44 +0200

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,12 +6,14 @@ export function HomePage() {
     <div>
       <div className='p-3 mb-4 bg-secondary d-flex align-items-center justify-content-between'>
         <h1 className='text-warning text-center m-0 flex-grow-1'>JobBoard</h1>
-        <img style={{ width: '55px', height: 'auto' }} src='/public/logo.png' />
+        <img style={{ width: '55px', height: 'auto' }} src='/logo.png' />
       </div>
 
       {/* Centered content */}
-      <div className='d-flex flex-column align-items-center justify-content-center text-center' style={{ height: '70vh' }}>
-        <h2 className='display-3 fw-bold mb-4'>Welcome</h2>
+      <div
+        className='d-flex flex-column align-items-center justify-content-center text-center'
+        style={{ height: '70vh' }}>
+        <h2 className='display-3 fw-bold mb-4'>Welcomeeeee</h2>
         <p className='fs-4 mb-5'>Welcome to the JobBoard application!</p>
         <h3 className='display-6 mb-4'>Are you a...</h3>
         <div className='d-flex gap-3'>
