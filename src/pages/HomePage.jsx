57786a015ffa67bb473ebe73b2032a1902333e8b
Yarin <yarinleib@gmail.com>
2025-05-13 19:35:30 +0200

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,7 +13,7 @@ export function HomePage() {
       <div
         className='d-flex flex-column align-items-center justify-content-center text-center'
         style={{ height: '70vh' }}>
-        <h2 className='display-3 fw-bold mb-4'>Welcomeeeee</h2>
+        <h2 className='display-3 fw-bold mb-4'>Welcome</h2>
         <p className='fs-4 mb-5'>Welcome to the JobBoard application!</p>
         <h3 className='display-6 mb-4'>Are you a...</h3>
         <div className='d-flex gap-3'>
